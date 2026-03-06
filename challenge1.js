ffe304e7bb21a9c12a8b8770ca3168d4511628ef
Taiyo Yamamoto <120293524+Ex0TiiC24@users.noreply.github.com>
2026-03-06 22:04:27 +0700

    add more

--- challenge1.js
+++ challenge1.js
@@ -1,14 +1,19 @@
 // Challenge 1: Write a for/while loop to console.log() odd numbers from 1 to 10
+// Expected output: 1 
+//                  3
+//                  5
+//                  7
+//                  9
 
 // Option 1: Using a for loop
 // for (...;...;...) {
-// 
+//
 // }
 
 // --------------------------------
 
 // Option 2: Using a while loop
-// let i = ... 
+// let i = ...
 // while (...) {
 //
 // }
